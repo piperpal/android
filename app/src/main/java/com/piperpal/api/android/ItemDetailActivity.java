12916de98aaf0040a4216c16542c4c795d64aa67
Ole Aamot <ole@aamotsoftware.com>
2019-01-17 06:25:44 +0100

    Piperpal 1.1 (20190117)

--- app/src/main/java/com/piperpal/api/android/ItemDetailActivity.java
+++ app/src/main/java/com/piperpal/api/android/ItemDetailActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -47,49 +48,51 @@ public class ItemDetailActivity extends AppCompatActivity {
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 
         fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                HTTPHandler sh = new HTTPHandler();
-                Context thisActivity = App.getContext();
-                String url = "https://piperpal.com/app/android"; // + mContentView.getText().toString();
-		String result = sh.makeServiceCall(url, HTTPHandler.GET);
-		if (ContextCompat.checkSelfPermission(thisActivity, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-		    Snackbar.make(view, result + "/" + getIntent().getStringExtra(ItemDetailFragment.ARG_ITEM_ID), Snackbar.LENGTH_LONG);
-		} else {
-		    Snackbar.make(view, "Location Permission Denied", Snackbar.LENGTH_SHORT)
-                            .setAction("Action", null).show();
+		@Override
+		public void onClick(View view) {
+		    // HTTPHandler sh = new HTTPHandler();
+		    // Context thisActivity = App.getContext();
+		    String url = "https://piperpal.com/market/";
+		    // String result = sh.makeServiceCall(url, HTTPHandler.GET);
+		    Snackbar.make(view, url, Snackbar.LENGTH_LONG).setAction("Action", new View.OnClickListener() {
+			    @Override
+			    public void onClick(View view) {
+				String webpage = "https://piperpal.com/market/?category=" + ItemDetailFragment.ARG_ITEM_ID;
+				Intent browserIntent = new Intent(Intent.ACTION_VIEW,
+								  Uri.parse(webpage));
+				startActivity(browserIntent);
+			    }
+			}).show();
+		    // Making a request to url and getting response
+		    // TextView mContentView = (TextView) view.findViewById(R.id.item_detail_container);
+		    // if (ContextCompat.checkSelfPermission(thisActivity, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+		    //     Snackbar.make(view, "https://piperpal.com/", Snackbar.LENGTH_LONG)
+		    //             .setAction("Action", null).show();
+		    // } else {
+		    //     Snackbar.make(view, "Location Permission Denied", Snackbar.LENGTH_SHORT)
+		    //             .setAction("Action", null).show();
+		    // }
+		    //Snackbar.make(view, "Mark your current geo with" + getIntent().getStringExtra(ItemDetailFragment.ARG_ITEM_ID.toString()), Snackbar.LENGTH_LONG)
+		    //        .setAction("Action", null).show();
+		    // HttpURLConnection urlConnection = null;
+		    // try {
+		    //    URL url = new URL("http://api.piperpal.com/location/push.php?service=" + ItemDetailFragment.ARG_ITEM_ID + "&glat=60&glon=10");
+		    //    urlConnection = (HttpURLConnection) url.openConnection();
+		    //    InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+		    //    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+		    //    StringBuilder result = new StringBuilder();
+		    //    String line;
+		    //    while ((line = reader.readLine()) != null) {
+		    //        result.append(line);
+		    //    }
+		    //    System.out.println(result.toString());
+		    // } catch (IOException e) {
+		    //    throw new RuntimeException(e);
+		    // } finally {
+		    //    urlConnection.disconnect();
+		    // }
 		}
-                // Making a request to url and getting response
-		// TextView mContentView = (TextView) view.findViewById(R.id.item_detail_container);
-                // if (ContextCompat.checkSelfPermission(thisActivity, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                //     Snackbar.make(view, "https://piperpal.com/", Snackbar.LENGTH_LONG)
-                //             .setAction("Action", null).show();
-                // } else {
-                //     Snackbar.make(view, "Location Permission Denied", Snackbar.LENGTH_SHORT)
-                //             .setAction("Action", null).show();
-                // }
-                //Snackbar.make(view, "Mark your current geo with" + getIntent().getStringExtra(ItemDetailFragment.ARG_ITEM_ID.toString()), Snackbar.LENGTH_LONG)
-                //        .setAction("Action", null).show();
-                // HttpURLConnection urlConnection = null;
-                // try {
-                //    URL url = new URL("http://api.piperpal.com/location/push.php?service=" + ItemDetailFragment.ARG_ITEM_ID + "&glat=60&glon=10");
-                //    urlConnection = (HttpURLConnection) url.openConnection();
-                //    InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-                //    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-                //    StringBuilder result = new StringBuilder();
-                //    String line;
-                //    while ((line = reader.readLine()) != null) {
-                //        result.append(line);
-                //    }
-                //    System.out.println(result.toString());
-                // } catch (IOException e) {
-                //    throw new RuntimeException(e);
-                // } finally {
-                //    urlConnection.disconnect();
-                // }
-
-            }
-        });
+	    });
 
         // Show the Up button in the action bar.
         ActionBar actionBar = getSupportActionBar();
